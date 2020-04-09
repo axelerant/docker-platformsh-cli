8d27f24f0d687f457b1447acd1446cddb5d10444
hw <hussainweb@gmail.com>
2020-04-09 15:45:50 -0400

    Add a simple build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+dir=$(dirname $0)
+
+set -ex
+
+export DOCKER_BUILDKIT=1
+
+docker pull pjcdawkins/platformsh-cli:latest
+
+docker build -t axelerant/platformsh-cli:latest ${dir}/
+
+docker push axelerant/platformsh-cli:latest
